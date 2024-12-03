a697d416823a0fa70f382ae4418835c2de9ce0ec
FlorencioFer <fur_oliveira@hotmail.com>
2024-12-02 23:24:43 -0300

    fix /payment parameter

--- src/main/kotlin/io/github/soat7/myburguercontrol/external/webservice/order/OrderController.kt
+++ src/main/kotlin/io/github/soat7/myburguercontrol/external/webservice/order/OrderController.kt
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -93,8 +94,8 @@ class OrderController(
         )
     }
 
-    @PostMapping("/payment", consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun sendPayment(@RequestBody orderId: String): ResponseEntity<Unit> {
+    @PostMapping("/{orderId}/payment", consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun sendPayment(@PathVariable orderId: String): ResponseEntity<Unit> {
         orderHandler.sendOrderPayment(UUID.fromString(orderId))
         return ResponseEntity.noContent().build()
     }
